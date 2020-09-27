eaf63dba47d627abb63d91259ac189c9a40a0dc3
= <ulim130@naver.com>
2020-09-27 20:32:55 +0900

    01-1 q4

--- src/chap01/c1_1.java
+++ src/chap01/c1_1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class c1_1 {
 	
-	// 네 값의 최댓값 구하기
+	//Q1 네 값의 최댓값 구하기
 	public static int q1(int a,int b,int c,int d){
 		int result = a;
 		if (result<b){
@@ -20,7 +20,7 @@ public class c1_1 {
 	}
 	
 	
-	// 세 값의 최소값 구하기
+	//Q2 세 값의 최소값 구하기
 	public static int q2(int a,int b,int c){
 		int result = a;
 		if(result>b){
@@ -32,7 +32,7 @@ public class c1_1 {
 		return result;
 	}
 	
-	// 네 값의 최소값 구하기
+	//Q3 네 값의 최소값 구하기
 	public static int q3(int a, int b, int c, int d){
 		int result = a;
 		if(result>b){
@@ -45,6 +45,21 @@ public class c1_1 {
 			result = d;
 		}
 		return result;
+	}
+	
+	//Q4 세 값의 중앙값 구하기
+	public static int q4(int a, int b, int c){
+		
+		if(a>b){
+			if(b>c) return b;
+			else if(a<c) return a;
+			else return c;
+		}
+		//여기서부터 a는 b보다 작다는게 이미 밝혀짐
+		else if(a>c) return a;
+		// 여기서부터는 a는 b와 c보다 작다는게 밝혀짐
+		else if(b>c) return c;
+		else return b;
 	}
 
 	public static void main(String[] args) {
@@ -61,6 +76,10 @@ public class c1_1 {
 		//q3
 		System.out.println("네 값의 최솟값을 구하기 위해 네개의 정수를 입력해주세요.");
 		System.out.println("최솟값 = "+ q3(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt()));
+		
+		//q4
+		System.out.println("세 값의 중앙값을 구하기 위해 세개의 정수를 입력해주세요.");
+		System.out.println("중앙값 = " + q4(sc.nextInt(),sc.nextInt(),sc.nextInt()));
 		
 		sc.close();
 	}
