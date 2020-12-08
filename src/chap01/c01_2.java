dba79cef7b66aac39b0ad6f740a6076f03b97b59
= <ulim130@naver.com>
2020-12-08 14:25:31 +0900

    01-2 q12

--- src/chap01/c01_2.java
+++ src/chap01/c01_2.java
@@ -99,6 +99,25 @@ public class c01_2 {
 		
 	}
 	
+	//곱셈 표 출력하기
+	public static void q12(){
+		
+		System.out.print("    |");
+		for (int i=1;i<10;i++){
+			System.out.printf("%3d",i);
+		}
+		System.out.println("\n---+------------------------------");
+		
+		for(int j=1;j<10;j++){
+			System.out.printf("%2d  | ",j);
+			for (int k=1;k<10;k++){
+				System.out.printf("%2d ",(j*k));
+			}
+			System.out.println();
+		}
+		
+	}
+	
 	public static void main(String args[]){
 		
 		Scanner sc = new Scanner(System.in);
@@ -137,6 +156,8 @@ public class c01_2 {
 		System.out.print("양의 정수: ");
 		int num = sc.nextInt();
 		q11(num);
+		
+		q12();
 		
 		sc.close();
 		
