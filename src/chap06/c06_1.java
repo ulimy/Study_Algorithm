4d7a8c7442fdffb32b631647f0ce46695905e3b7
= <ulim130@naver.com>
2020-12-28 14:39:56 +0900

    06_1

--- src/chap06/c06_1.java
+++ src/chap06/c06_1.java
@@ -1,11 +1,39 @@
 package chap06;
 
+import java.util.Arrays;
+
 public class c06_1 {
-	/*버블정렬*/
+	/*버블정렬
+	 맨 앞 혹은 맨 뒤 에섭터 이웃한 요소를 비교, 교환
+	  */
+	
+	static int[] arr = {5,7,2,4,0,1};
+	
+	static void swap(int a, int b){
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+		return;
+	}
+	
+	static void BubbleSort(){
+		for (int i=0;i<arr.length;i++){
+			int exchg=0; // 반복 끝났는데 교횐 횟수 0이라면 이후 교환은 할 필요 X
+			for (int j=arr.length-1;j>i;j--){
+				if(arr[j]<arr[j-1]){
+					swap(j,j-1);
+					exchg++;
+				}
+			}
+			if (exchg==0) {break;}
+		}
+		return;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BubbleSort();
+		System.out.println(Arrays.toString(arr));
+		return;
 	}
 
 }
